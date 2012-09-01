5f906f5fd24b165d2efae675a46364046446d9f4
Eraskin Alexei <levrun1@gmail.com>
2012-09-02 02:36:00 +0400

    lesson1 - dz1

---
+++ src/edu/java/september2012/lesson1/eraskin/HelloWorld.java
@@ -0,0 +1,9 @@
+package edu.java.september2012.lesson1.eraskin;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+
+}
